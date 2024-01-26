ad78bf49a95dea17de906cff0972f05b80fbe200
sateeshchikkalagi <sateesh.c@pepperfry.com>
2024-01-26 18:30:56 +0530

    Bottom nav with navigation drawer

--- app/src/main/java/com/navigationgraph/ui/MainActivity.kt
+++ app/src/main/java/com/navigationgraph/ui/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.navigationgraph.R
 import com.navigationgraph.databinding.ActivityMainBinding
@@ -11,13 +14,37 @@ import com.navigationgraph.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment: NavHostFragment = supportFragmentManager.findFragmentById(R.id.flContainer) as NavHostFragment
-        val navController: NavController = navHostFragment.navController
+        initializeToolbar()
+        initBottomNavigation()
+        initializeNavigationDrawer()
+    }
+
+    private fun initializeToolbar() {
+        setSupportActionBar(binding.commonToolbar.toolbar)
+    }
+
+    private fun initializeNavigationDrawer() {
+        binding.navView.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    private fun initBottomNavigation() {
+        val navHostFragment: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.flContainer) as NavHostFragment
+        navController = navHostFragment.navController
         binding.bottomNav.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/navigationgraph/ui/MenuFragment.kt
@@ -0,0 +1,30 @@
+package com.navigationgraph.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.navigationgraph.R
+import com.navigationgraph.databinding.FragmentMenuBinding
+
+class MenuFragment : Fragment() {
+
+    private lateinit var binding: FragmentMenuBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMenuBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvMenu.setOnClickListener {
+            findNavController().navigate(R.id.menuFragment_to_editAccountFragment)
+        }
+    }
+}
